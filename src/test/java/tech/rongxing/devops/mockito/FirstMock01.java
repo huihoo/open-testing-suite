d653f881fdf6d8fb2e5715647f1b43f271dcca92
huihoo <ihuihoo@gmail.com>
2021-07-01 11:25:37 +0800

    add mockito test

---
+++ src/test/java/tech/rongxing/devops/mockito/FirstMock01.java
@@ -0,0 +1,28 @@
+package tech.rongxing.devops.mockito;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import java.util.List;
+import org.junit.Test;
+
+public class FirstMock01 {
+    @Test
+    public void test() {
+        // mock creation
+        @SuppressWarnings("unchecked")
+        List<String> mockedList = mock(List.class);
+
+        // using mock object
+        mockedList.add("one");
+        mockedList.add("two");
+        mockedList.add("three");
+        mockedList.clear();
+
+        // verification
+        verify(mockedList).clear();
+        verify(mockedList).add("one");
+        verify(mockedList).add("two");
+        verify(mockedList).add("Three");
+    }
+}
---
+++ src/test/java/tech/rongxing/devops/mockito/SecondStubbing02.java
@@ -0,0 +1,38 @@
+package tech.rongxing.devops.mockito;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.LinkedList;
+import org.junit.Test;
+
+public class SecondStubbing02 {
+    @Test
+    public void test() {
+        // You can mock concrete classes, not only interfaces
+        @SuppressWarnings("unchecked")
+        LinkedList<String> mockedList =(LinkedList<String>) mock(LinkedList.class);
+
+        // stubbinging
+        when(mockedList.get(0)).thenReturn("first");
+        when(mockedList.get(1)).thenThrow(new RuntimeException());
+
+        // following prints "first"
+        System.out.println(mockedList.get(0));
+
+        // following throws runtime exception
+        try{
+            System.out.println(mockedList.get(1));
+            fail("Exception was not throw");
+        }catch (Exception e){
+
+        }
+        // following prints "null" because get(999) was not stubbed
+        System.out.println(mockedList.get(999));
+
+        verify(mockedList).get(0);
+
+    }
+}
